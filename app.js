bdd5a6468b93fabb2cf23bd9e9c58b93a7f0a3a4
Eduardo Brennand <eduardolirabrennand@gmail.com>
2022-02-01 18:50:33 -0300

    Corrige o contador e torna ele funcional

--- app.js
+++ app.js
@@ -4,36 +4,30 @@ document.addEventListener('DOMContentLoaded', function() {
     botao.addEventListener('click', converteHoras);
 
 
+    function convertDateToUTC(date) { 
+        return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds()); 
+        }
+
     function converteHoras(){
         var getHora = document.querySelector('.desafio-contador-horas');
         var getMinuto = document.querySelector('.desafio-contador-minutos');
         var getSegundo = document.querySelector('.desafio-contador-segundos');
 
 
+        
         var dataAtualTime = new Date().getTime();
-        var dataInput = new Date(document.querySelector('.desafio-input-date').value);
-        dataInput.setHours(0,0,0,0);
+        var dataInput = convertDateToUTC(new Date(document.querySelector('.desafio-input-date').value));
         var dataInputTime = dataInput.getTime();
-
-        var diferenca = dataInputTime - dataAtualTime;
+        var diferenca = (dataInputTime - dataAtualTime) / 1000;
     
-        var horas = Math.floor((diferenca % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutos = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60));
-        var segundos = Math.floor((diferenca % (1000 * 60)) / 1000);
+       var horas = Math.floor(diferenca / 3600);
+       var minutos = Math.floor((diferenca - horas * 3600) / 60);
+       var segundos = Math.floor((diferenca - (minutos * 60) - (horas * 3600)));
 
         getHora.innerHTML = horas + ':';
         getMinuto.innerHTML = minutos + ':';
         getSegundo.innerHTML = segundos;
 
         timer = setInterval(converteHoras, 1000);
-        
     }
-
-    
-
-
-
-    
-
-
 })
