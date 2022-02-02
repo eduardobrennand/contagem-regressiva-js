e548f3bf55ad1614c5a0a8b10449a14a50bde8f3
Eduardo Brennand <eduardolirabrennand@gmail.com>
2022-02-02 11:53:04 -0300

    Adiciona a funcao de localStorage, adiciona :hover e :active no botao e modifica algumas partes do script JS

--- app.js
+++ app.js
@@ -1,33 +1,53 @@
 document.addEventListener('DOMContentLoaded', function() {
     var botao = document.querySelector('.desafio-button');
 
+    var getHora = document.querySelector('.desafio-contador-horas');
+    var getMinuto = document.querySelector('.desafio-contador-minutos');
+    var getSegundo = document.querySelector('.desafio-contador-segundos');
+
     botao.addEventListener('click', converteHoras);
+    
 
+    if (localStorage.length < 0) {
+        localStorage.setItem('dataInput', dataInputStorage);
+    } else {
+        document.querySelector('.desafio-input-date').value = localStorage.getItem('dataInput')
+    }
 
-    function convertDateToUTC(date) { 
+    function converteDataUTC(date) { 
         return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds()); 
         }
 
     function converteHoras(){
-        var getHora = document.querySelector('.desafio-contador-horas');
-        var getMinuto = document.querySelector('.desafio-contador-minutos');
-        var getSegundo = document.querySelector('.desafio-contador-segundos');
 
-
-        
         var dataAtualTime = new Date().getTime();
-        var dataInput = convertDateToUTC(new Date(document.querySelector('.desafio-input-date').value));
+        var dataInput = converteDataUTC(new Date(document.querySelector('.desafio-input-date').value));
         var dataInputTime = dataInput.getTime();
+        var dataInputStorage = document.querySelector('.desafio-input-date').value
         var diferenca = (dataInputTime - dataAtualTime) / 1000;
-    
-       var horas = Math.floor(diferenca / 3600);
-       var minutos = Math.floor((diferenca - horas * 3600) / 60);
-       var segundos = Math.floor((diferenca - (minutos * 60) - (horas * 3600)));
+       
+        
+        
+        var horas = Math.floor(diferenca / 3600);
+        var minutos = Math.floor((diferenca - horas * 3600) / 60);
+        var segundos = Math.floor((diferenca - (minutos * 60) - (horas * 3600)));
 
         getHora.innerHTML = horas + ':';
         getMinuto.innerHTML = minutos + ':';
-        getSegundo.innerHTML = segundos;
+        getSegundo.innerHTML = segundos;   
+        
+        localStorage.setItem('dataInput', dataInputStorage);
 
         timer = setInterval(converteHoras, 1000);
-    }
+
+
+    } 
+
+    
+    
+    
+        
+
+    
+    
 })
